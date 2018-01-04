9dbae93713d1b39e2bb400311d826e4ee3e67d80
valentine herpreck <valentineherpreck@MacBook-Air-de-valentine.local>
2018-01-04 17:49:35 +0100

    puh

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http') // recupérer une adresse http 
 var express = require('express');
 var app = express();
+var mongo = require('./mongo.js');
 
 //const server = http.createServer((req, res) => {
 	//res.statusCode = 200;
@@ -9,12 +10,17 @@ var app = express();
 
 //}); 
 
+// recupération de la var environnement Clever 
+var uri = process.env.MONGODB_ADDON_URI;
 var http = require('http').Server(app);
 
 app.get('/hello', function(req, res){
 	res.end('Salut tout le monde');
 })
 
+mongo.connect(uri, function()}
+conso.log("MongoDB connected");
 http.listen(8080, () =>{
 	console.log("server started");
+});
 })
